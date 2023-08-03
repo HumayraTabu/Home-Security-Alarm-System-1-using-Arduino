b78aa0efb4ed4c2905c17f37636ffd61397b60dd
Humayra Tabassum <77066946+HumayraTabu@users.noreply.github.com>
2023-08-03 12:20:44 +0600

    Add files via upload

---
+++ intruder_security/intruder_security.ino
@@ -0,0 +1,32 @@
+int ledPin = 8;                
+int inputPin = 2;              
+int pirState = LOW;             
+int val = 0;                    
+ 
+void setup() {
+  pinMode(ledPin, OUTPUT);      
+  pinMode(inputPin, INPUT);     
+ 
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  val = digitalRead(inputPin);  
+  if (val == HIGH) {            
+    digitalWrite(ledPin, HIGH);  
+    if (pirState == LOW) {
+      
+      Serial.println("Intruder Detected");
+      
+      pirState = HIGH;
+    }
+  } else {
+    digitalWrite(ledPin, LOW); 
+    if (pirState == HIGH){
+    
+      Serial.println("No Intruder");
+      
+      pirState = LOW;
+    }
+  }
+}
